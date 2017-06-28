bf0004307e9346fc519d9e5f4acbb7c507a02543
kims <kuzbyt@ukr.net>
2017-06-28 22:26:02 +0300

    Iнкапсуляція

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -11,6 +11,16 @@ namespace snake
         public int x;
         public int y;
         public char sym;
+       /* public Point()
+            {
+           Console.WriteLine("Стоврюється нова точка");
+            }*/
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
