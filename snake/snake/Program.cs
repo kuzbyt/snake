aaff107614758975bc22b7c693f2ad4b3931ea96
kims <kuzbyt@ukr.net>
2017-07-02 23:13:29 +0300

    Горизонтальная линия

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,35 +13,16 @@ namespace snake
         {
             Point p1 = new Point(1,3,'*');
             
-              /*  p1.x = 1;
-                p1.y = 3;
-                p1.sym = '*';*/
-                p1.Draw(); ;
+                 p1.Draw(); ;
 
             Point p2 = new Point(4,5,'+');
 
-              /*  p2.x = 4;
-                p2.y = 5;
-                p2.sym = '+';*/
+                 p2.Draw();
 
-                p2.Draw();
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+                line.Drow();
 
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            numList.RemoveAt(0);
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
 
             Console.ReadLine();
 
